63ecf230c66843301a96296d6908fc9771fcb4b2
Левкин Сергей <36307272+LevkinSergey@users.noreply.github.com>
2025-01-31 23:44:12 +0300

    Сообщить("КаталогЕДТ:"+КаталогЕДТ);

--- build_actions.bat
+++ build_actions.bat
@@ -1,2 +1,2 @@
 chcp 65001
-oscript.exe  .\src\builder\build.os --edtSource="C:\Program Files\1C\1CE\components\1c-edt-2024.1.3+13-x86_64\" --platformSource="C:\Program Files\1cv8\8.3.14.2095\bin\" 
+oscript.exe -encoding=utf-8 .\src\builder\build.os --edtSource="C:\Program Files\1C\1CE\components\1c-edt-2024.1.3+13-x86_64\" --platformSource="C:\Program Files\1cv8\8.3.14.2095\bin\" 
