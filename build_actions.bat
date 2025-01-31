80d959e9c685ee8b9a2678b7b7f7a04ae6281dbb
Левкин Сергей <36307272+LevkinSergey@users.noreply.github.com>
2025-01-31 14:53:54 +0300

    build_actions.bat

---
+++ build_actions.bat
@@ -0,0 +1,3 @@
+chcp 65001
+@oscript.exe  .\src\builder\build.os --platformSource="C:\Program Files\1cv8\8.3.14.2095\bin\" --edtSource="C:\Program Files\1C\1CE\components\1c-edt-2024.1.3+13-x86_64\"
+@exit /b %ERRORLEVEL%
