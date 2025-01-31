37ee7e051ea6a7504e688bbefb0f6770d34beab0
Левкин Сергей <36307272+LevkinSergey@users.noreply.github.com>
2025-01-31 23:37:41 +0300

    pwsh

--- build_actions.bat
+++ build_actions.bat
@@ -1,3 +1,2 @@
 chcp 65001
-@oscript.exe  .\src\builder\build.os --platformSource="C:\Program Files\1cv8\8.3.14.2095\bin\" --edtSource="C:\Program Files\1C\1CE\components\1c-edt-2024.1.3+13-x86_64\"
-@exit /b %ERRORLEVEL%
+oscript.exe  .\src\builder\build.os --edtSource="C:\Program Files\1C\1CE\components\1c-edt-2024.1.3+13-x86_64\" --platformSource="C:\Program Files\1cv8\8.3.14.2095\bin\" 
